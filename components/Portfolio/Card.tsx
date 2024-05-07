f74c718c8cce89343ab1cc8dab812fb139710faa
prajjwal <howtoshout@gmail.com>
2024-05-08 00:02:02 +0530

    portfolio grid

--- components/Portfolio/Card.tsx
+++ components/Portfolio/Card.tsx
@@ -3,13 +3,20 @@ import React from "react";
 
 export default function Card() {
   return (
-    <div className="portfolio-card min-w-60 h-full min-h-40 rounded-lg relative">
-      <Image className="w-full h-full cover" src="/word1.png" alt="my work" width={500} height={500} clas />
-      <div  className="flex flex-col gap-5 text-white text-sm absolute z-50 items-center justify-between w-full h-full  ">
-        <span className="rounded-full border border-white">Branding</span>
-        <span className="font-bol">Lead Generation Campaign</span>
+    <div className="portfolio-card min-w-60 h-full min-h-40 rounded-lg relative group transition-all duration-200 cursor-pointer">
+      <Image
+        className="w-full h-full cover"
+        src="/word1.png"
+        alt="my work"
+        width={500}
+        height={500}
+      />
+      <span className="absolute top-0 transition-all duration-200 left-0 h-full w-full bg-black opacity-40 z-10 hidden peer-hover:block group-hover:block"></span>
+
+      <div className="flex flex-col gap-5 text-white absolute left-0 top-0 z-50 items-center opacity-0 group-hover:opacity-100 justify-center w-full h-full  transition-all duration-200 ">
+        <span className="rounded-full border border-white px-4 py-2">Branding</span>
+        <span className="font-bold">Lead Generation Campaign</span>
       </div>
-      <span className="absolute top-0 left-0 h-full w-full bg-black opacity-40 z-10"></span>
     </div>
   );
 }
--- components/Portfolio/Grid.tsx
+++ components/Portfolio/Grid.tsx
@@ -1,14 +1,18 @@
 import React from "react";
 import Card from "./Card";
+import Button from "../Button";
 
 export default function Grid() {
   return (
     <section className="relative z-50 flex items-center justify-center">
-      <div className="max-w-6xl">
-        <div className="grid grid-cols-4 gap-1 portfolio-grid ">
+      <div className="max-w-6xl flex flex-col items-center justify-center">
+        <div className="grid grid-cols-4 gap-1 portfolio-grid mb-10">
           <Card /> <Card /> <Card /> <Card /> <Card />
-          <Card /> <Card /> <Card /> <Card /> <Card /><Card /><Card />
+          <Card /> <Card /> <Card /> <Card /> <Card />
+          <Card />
+          <Card />
         </div>
+          <Button variant="Outlined" title="View all" />
       </div>
     </section>
   );
